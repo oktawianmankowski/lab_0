674b3795d3a0f08068c9859a80335d210f94f035
Oktawian.Mankowski <198162@edu.p.lodz.pl>
2017-11-25 17:51:22 +0100

    push lab_0

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -8,7 +8,7 @@ public class TailsGenerator {
 	public List<String> tails(String value) {
 		List<String> tails = new ArrayList<>(value.length());		
 		for(int i=0; i<value.length()+1; i++) {
-		    tails.add(value.substring(i,  value.length()));
+		    tails.add(value.substring(i, value.length()))
 		}
 		
 		return tails;
